7e1a212dc84f1eb450779d2d254f50dccc3ac725
himaji <himaji.mac@gmail.com>
2021-03-30 16:35:56 +0900

    header and footer source migration
    元ソースからヘッダー、フッターとホーム画面の切り分け、移行の完了

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.common')
+
+@section('content')
+  <div class="parallax-window" data-parallax="scroll" data-image-src="image/test_main.jpg" style="height: 200px; background-attachment: fixed;">
+    <p>パララックス</p>
+  </div>
+
+  <div class="w-auto bg-warning" style="height: 200px;">
+    PsychologyTest
+  </div>
+
+  <div class="parallax-window" data-parallax="scroll" data-image-src="image/test_main.jpg" style="height: 200px; background-attachment: fixed;">
+    <p>パララックス</p>
+  </div>
+
+  <div class="w-auto bg-warning" style="height: 200px;">
+    Content
+  </div>
+
+  <div class="parallax-window" data-parallax="scroll" data-image-src="image/test_main.jpg" style="height: 200px; background-attachment: fixed;">
+    <p>パララックス</p>
+  </div>
+
+  <div class="w-auto bg-warning" style="height: 200px;">
+    Care
+  </div>
+@endsection
+
+@section('js')
+
+@endsection
