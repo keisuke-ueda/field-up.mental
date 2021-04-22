45924e4fef596f8f1be6f57686715db899ac7861
himaji <himaji.mac@gmail.com>
2021-04-22 18:33:37 +0900

    fix
    名前付きルート修正

--- app/Http/Controllers/ContentsListController.php
+++ app/Http/Controllers/ContentsListController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContentsListController extends Controller
 {
     public function show() {
-        return view('mental_field-up/contents_list');
+        return view('contents_list');
     }
 }
