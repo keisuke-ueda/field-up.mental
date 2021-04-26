8ba8e1f823c1e6a5efc1daba45cac27fde8fd287
himaji <himaji.mac@gmail.com>
2021-04-26 14:25:39 +0900

    fix
    微修正

--- resources/views/psychology_test/egogram.blade.php
+++ resources/views/psychology_test/egogram.blade.php
@@ -122,9 +122,9 @@
 @endsection
 
 @section('js')
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <script>
 $(function () {
@@ -135,7 +135,9 @@ $(function () {
     $('#modalArea').fadeOut();
   });
 });
+</script>
 
+<script>
 let score_list = new Array(5);
 
 let data = {
