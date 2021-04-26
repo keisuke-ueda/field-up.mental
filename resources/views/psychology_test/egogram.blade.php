c3a73cac498a2d64fd37db32d26598887ce9eb04
himaji <himaji.mac@gmail.com>
2021-04-26 15:32:09 +0900

    fix
    修正

--- resources/views/psychology_test/egogram.blade.php
+++ resources/views/psychology_test/egogram.blade.php
@@ -211,18 +211,24 @@ function OnButtonClick() {
   $(function () {
     $('#button-1').click(function() {
       alert('ボタンテスト');
-    })
-  })
+    });
+  });
   
   $(function () {
     $('#openModal').click(function(){
         $('#modalArea').fadeIn();
     });
+    // $('#closeModal , #modalBg').click(function(){
+    //   $('#modalArea').fadeOut();
+    // });
+  });
+
+  $(function () {
     $('#closeModal , #modalBg').click(function(){
       $('#modalArea').fadeOut();
     });
   });
-  </script>
+</script>
 @endsection
 
 @section('title')
