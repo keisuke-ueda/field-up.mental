625724719cc83f03c4284ef755c86457f308c454
himaji <himaji.mac@gmail.com>
2021-04-26 14:11:11 +0900

    fix
    微調整

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -31,7 +31,7 @@
         @include('parts.header')
         @yield('content')
         @include('parts.footer')
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
         @yield('js')
     </body>
 </html>
