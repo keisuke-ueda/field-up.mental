afe31515df200e50cb2a1d8d598d29187b2c22f2
himaji <himaji.mac@gmail.com>
2021-04-26 14:16:56 +0900

    fix
    微調整

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -31,7 +31,6 @@
         @include('parts.header')
         @yield('content')
         @include('parts.footer')
-        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
         @yield('js')
     </body>
 </html>
--- resources/views/psychology_test/egogram.blade.php
+++ resources/views/psychology_test/egogram.blade.php
@@ -122,9 +122,9 @@
 @endsection
 
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
 
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <script>
 $(function () {
