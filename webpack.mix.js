83a93b0abf90ee10fa1a3bb4042cdafe1c76bf85
himaji <himaji.mac@gmail.com>
2021-04-28 17:24:58 +0900

    edit css
    画像関連項目のcommon.cssへの移動

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
+    .setResourceRoot("")
     .sass('resources/sass/app.scss', 'public/css');
 
 // saas導入
