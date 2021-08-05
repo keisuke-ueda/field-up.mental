b4ff046d06faac2ffa11b4bfebfc7536a3970b1f
keisuke-ueda <81659758+keisuke-ueda@users.noreply.github.com>
2021-08-05 23:22:02 +0900

    8/5_2

--- resources/views/contents/co_sec6.blade.php
+++ resources/views/contents/co_sec6.blade.php
@@ -17,7 +17,7 @@
       <div class="col-md-4 item mb-5">
         <div class="card" style="margin: 10px;">
           <a href='{{ route("contents_sec.select", ["page_number"=>601])}}' class="w-100 h-100">
-            <img src='{{ asset("/image/sec6_image/色とイメージ(1).jpg") }}' class="card-img" alt="...">
+            <img src='{{ asset("/image/sec6_image/色とイメージ(1).jpg")}}' class="card-img" alt="...">
             <h3 class="card-title">【色とイメージ】みんなは色にどんなイメージを持っていますか？</h3>
           </a>
         </div>
@@ -25,9 +25,9 @@
       
       <div class="col-md-4 item mb-5">
         <div class="card" style="margin: 10px;">
-          <a href='{{ route("making_now.show") }}' class="w-100 h-100">
-            <img src='{{ asset("/image/製作中ページ.jpg") }}' class="card-img" alt="...">
-              <h3 class="card-title">【準備中】ちょっと待っててね</h3>
+          <a href='{{ route("contents_sec.select", ["page_number"=>602])}}' class="w-100 h-100">
+            <img src='{{ asset("/image/sec6_image/色と理論(2).jpg")}}' class="card-img" alt="...">
+              <h3 class="card-title">【色と理論】普段使っている色の使いかを見直してみよう</h3>
               <p class="card-text">
               </p>
           </a>
