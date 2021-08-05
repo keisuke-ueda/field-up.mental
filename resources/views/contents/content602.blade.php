f2ffd25c954cbb1e4c09f6d7afc5a1f04e011334
keisuke-ueda <81659758+keisuke-ueda@users.noreply.github.com>
2021-08-05 23:12:37 +0900

    8/5

---
+++ resources/views/contents/content602.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.common')
+
+@section('content')
+<div id="mental_care_contents">
+  <div class="row justify-content-center container mx-auto">
+    <div class="col-md-7 page-title  mt-5 pb-3">
+      <h1 class="text-center">色の理論</h1>
+    </div>
+  </div>
+
+  <div class="row justify-content-center container mx-auto">
+    <div class="col-md-10 item content-lead-text mt-3 pb-3">
+      <p>どのように色をつかってけばいのかがわからない<br>
+        デザインの仕方がわからないといったようなことはありませんか？<br>
+        同じ赤色でもトーンを変えていくと印象が全然変わりますよ</p>
+    </div>
+
+    <div class="col-md-10 item text-center youtube mb-5">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Tx9ivxL9qY0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <div class="container row">
+      <div class="col m-4" style="height: 250px;">
+        <img class="h-100" src="{{ asset('image/contents_img/content602/1-1.jpg')}}" />
+      </div>
+
+      <div class="col m-4" style="height: 250px;">
+        <img class="h-100" src="{{ asset('image/contents_img/content602/1-2.jpg')}}" />
+      </div>
+
+      <div class="col m-4" style="height: 250px;">
+        <img class="h-100" src="{{ asset('image/contents_img/content602/1-3.jpg')}}" />
+      </div>
+
+      <div class="col m-4" style="height: 250px;">
+        <img class="h-100" src="{{ asset('image/contents_img/content602/1-4.jpg')}}" />
+      </div>
+    </div>
+
+  </div>
+
+</div>
+@endsection
+
+@section('js')
+@endsection
+
+@section('title')
+ココロとカラダの関係
+@endsection
