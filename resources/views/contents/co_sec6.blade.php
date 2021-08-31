32a542aa03fb20e0dddba7bd07b078aa04ae2605
keisuke-ueda <keisuke.ueda@field-up.work>
2021-09-01 04:07:27 +0900

    再更新9.1

--- resources/views/contents/co_sec6.blade.php
+++ resources/views/contents/co_sec6.blade.php
@@ -36,7 +36,7 @@
 
       <div class="col-md-4 item mb-5">
         <div class="card" style="margin: 10px;">
-          <a href='{{ route("making_now.show") }}' class="w-100 h-100">
+        <a href='{{ route("contents_sec.select", ["page_number"=>603])}}' class="w-100 h-100">
             <img src='{{ asset("/image/色とデザイン(3).jpg") }}' class="card-img" alt="...">
               <h3 class="card-title">【色とデザイン】デザインて実は心理学なんですよ♪♪</h3>
               <p class="card-text">
