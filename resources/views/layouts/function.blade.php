6573f945e3442c3e8d81404790c68849288de2e2
keisuke-ueda <81659758+keisuke-ueda@users.noreply.github.com>
2022-10-10 22:08:39 +0900

    ページ修正

--- resources/views/layouts/function.blade.php
+++ resources/views/layouts/function.blade.php
@@ -8,6 +8,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href='{{ asset("/image/ファビコン.png")}}'>
+    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
